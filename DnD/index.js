10fd3031ded07e301fc107291afa615eab555460
Enzo-Gpp <enzogabrielporfirio09@gmail.com>
2025-05-31 18:49:06 -0300

    fafdaqdf
    daddada

---
+++ DnD/index.js
@@ -0,0 +1,13 @@
+let NotadoAluno = 9.0
+if (NotadoAluno > 7){
+console.log(`a nota do aluno foi ${NotadoAluno}, parabéns, ele passou`)
+}
+else{
+    console.log("Infelizmente a nota do aluno foi",NotadoAluno,",ele pode tentar de novo na rec")
+}
+if (NotadoAluno == 9 == true){
+    console.log("Seu filho passou para olímpico")
+}
+else{
+    console.log(" Seu filho vai para a turma da frei")
+}
